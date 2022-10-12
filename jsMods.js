266f7f0aa29a4fa4f36888d221547c21b1adee11
Oliver Crosby <42810349+OliverCrosby@users.noreply.github.com>
2022-10-12 11:22:49 -0400

    added JS file

---
+++ jsMods.js
@@ -0,0 +1,13 @@
+/*
+Clean slate:
+1. create div at top of body. This will contain the whole new design
+2. append elements I wanna keep to that div (or right into their appropriate place)
+    #content
+
+3. wrap remaining elements into container
+4. hide that container
+// look at my clean slate thing for gcpedia.
+
+*/
+
+console.log('hey sexy')
